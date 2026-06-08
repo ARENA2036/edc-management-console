8e38551b06b0517e15c36023eefd3936d4f7d65c
Owais Khan <owais.khan@arena2036.de>
2026-06-08 11:56:20 +0200

    chore(DT-55): ensure Helm requirements for A36X-EMC

--- backend/scripts/setup_kube.sh
+++ backend/scripts/setup_kube.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -e
 
-# Ensure environment variables are set
-if [ -z "$AZURE_CLIENT_ID" ] || [ -z "$AZURE_CLIENT_SECRET" ] || [ -z "$AZURE_TENANT_ID" ] || [ -z "$AKS_CLUSTER_NAME" ] || [ -z "$AKS_RESOURCE_GROUP" ]; then
-  echo "One or more environment variables are missing!"
-  exit 1
-fi
+# ---- CONFIG ----
+VAULT_ADDR=""
+VAULT_TOKEN=""   # export VAULT_TOKEN before running
+SECRET_PATH="staging/data/cluster"  # KV v2 path
+SECRET_KEY="config"
+CLUSTER_NAME="staging"
 
-# Check if the Azure CLI is installed
-if ! command -v az &> /dev/null; then
-  echo "Azure CLI (az) could not be found, please install it."
-  exit 1
-fi
+
 
 # Check if kubectl is installed
 if ! command -v kubectl &> /dev/null; then
@@ -19,21 +16,14 @@ if ! command -v kubectl &> /dev/null; then
   exit 1
 fi
 
-# Login to Azure using Service Principal
-echo "Logging into Azure..."
-az login --service-principal \
-  -u "$AZURE_CLIENT_ID" \
-  -p "$AZURE_CLIENT_SECRET" \
-  --tenant "$AZURE_TENANT_ID"
-
 # Fetch kubeconfig inside container (put in non-root home)
-echo "Fetching kubeconfig for AKS cluster..."
+echo "Fetching kubeconfig for the cluster..."
 mkdir -p /home/nonroot/.kube
-az aks get-credentials \
-  --resource-group "$AKS_RESOURCE_GROUP" \
-  --name "$AKS_CLUSTER_NAME" \
-  --file /home/nonroot/.kube/config \
-  --overwrite-existing
+vault login -address=$VAULT_ADDR -method=token $VAULT_TOKEN
+vault kv get -field=config $SECRET_PATH > /home/nonroot/.kube/config
+
+# vault kv get -field=json $SECRET_PATH | jq -r ".data.data.$SECRET_KEY" > /home/nonroot/.kube/config
+
 
 # Persist KUBECONFIG for all future shells
 echo 'export KUBECONFIG=/home/nonroot/.kube/config' >> /home/nonroot/.bashrc
@@ -44,14 +34,14 @@ echo "Available contexts:"
 kubectl config get-contexts
 
 # Check if the context exists
-if ! kubectl config get-contexts | grep -q "$AKS_CLUSTER_NAME"; then
-  echo "Context '$AKS_CLUSTER_NAME' not found!"
+if ! kubectl config get-contexts | grep -q "$CLUSTER_NAME"; then
+  echo "Context '$CLUSTER_NAME' not found!"
   exit 1
 fi
 
 # Set the current context to the dynamically passed AKS_CLUSTER_NAME
-echo "Setting current context to '$AKS_CLUSTER_NAME'..."
-kubectl config use-context "$AKS_CLUSTER_NAME" || { echo "Failed to set context!"; exit 1; }
+echo "Setting current context to '$CLUSTER_NAME'..."
+kubectl config use-context "$CLUSTER_NAME" || { echo "Failed to set context!"; exit 1; }
 
 # Verify the current context
 echo "Current kubectl context:"
